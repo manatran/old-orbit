901a9fa82cb35925e7677fbdbf65c0539409d7d4
Manaus Transez <manatran@student.arteveldehs.be>
2019-03-03 00:16:38 +0100

    Updated Question component

--- client/src/components/questions/index.js
+++ client/src/components/questions/index.js
@@ -4,28 +4,30 @@ import './questions.css';
 class Question extends Component {
   render() {
     return (
-      <section className="question">
+      <div className="question">
         <div className="votes">
-          <i className="material-icons up">keyboard_arrow_up</i>
+          <i className="material-icons up">arrow_drop_up</i>
           <span>{this.props.likes}</span>
-          <i className="material-icons down">keyboard_arrow_down</i>
+          <i className="material-icons down">arrow_drop_down</i>
         </div>
 
         <img src={this.props.thumbnail} alt={this.props.category} />
 
-        <div className="question-body">
+        <a href="/" className="question-body">
           <h2>{this.props.title} <span className="tag">{this.props.tag}</span></h2>
           <p className="light">
-            <span className="category">{this.props.category}</span>
+            <span className="category"><img src={this.props.thumbnail} alt={this.props.category} />{this.props.category}</span>
             Asked 5 minutes ago by
-            <a href="/" className="author"><i className="material-icons admin">verified_user</i>{this.props.author}</a></p>
-        </div>
+            <span className="author"><i className="material-icons admin">verified_user</i>{this.props.author}</span></p>
+        </a>
 
         <div className="options">
-          <span className="comments">{this.props.comments}<i className="material-icons">comments</i></span>
-          <span className="more"><i className="material-icons">more_horiz</i></span>
+          <a href="/" className="comments">{this.props.comments}<i className="material-icons">comments</i></a>
+          <span onClick={() => {
+            console.log('clicked');
+          }} className="more"><i className="material-icons">more_horiz</i></span>
         </div>
-      </section>
+      </div>
     );
   }
 }
